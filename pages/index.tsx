b3a0bf9ccb8154a64653886b2f7542a5205f0d06
killosismo <killosismo>
2023-03-22 01:02:17 -0400

    hope final comminininininini

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ import { fetchForInvestors } from '../utils/fetchForInvestors'
 import { fetchProperty } from '../utils/fetchProperty'
 import { fetchTown } from '../utils/fetchTown'
 
-
+/*
 type Props = {
   properties: Property[];
   forInvestorss: ForInvestors[];
@@ -21,13 +21,14 @@ type Props = {
   
 };
 
+*/
 
 
-
-const Home = ( { properties, forInvestorss, towns }: Props) => {
+const Home = ( /*{ properties, forInvestorss, towns }: Props*/) => {
   
   return (
-    <div className="">
+    <h1>la</h1>
+    /*<div className="">
       <Head>
         <title>Beachland Properties</title>
         <link rel="icon" href="/favicon.ico" />
@@ -124,12 +125,13 @@ const Home = ( { properties, forInvestorss, towns }: Props) => {
       </main>
       <Footer />
     </div>
+    */
   )
 }
 
 export default Home;
 
-  export const getStaticProps: GetStaticProps<Props> = async () => {
+  /*export const getStaticProps: GetStaticProps<Props> = async () => {
   const forInvestorss: ForInvestors[] = await fetchForInvestors();
   const properties: Property[] = await fetchProperty();
   const towns: Town[] = await fetchTown();
@@ -142,7 +144,7 @@ export default Home;
     },
     revalidate: 10,
   }
-}
+}*/
   
 
 
