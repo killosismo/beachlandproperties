a690bae28f35101fd39b4424f5323ed046ac1882
killosismo <killosismo>
2023-03-27 21:51:20 -0400

    final 23

--- pages/forInvestors/[slug].tsx
+++ pages/forInvestors/[slug].tsx
@@ -139,7 +139,7 @@ export const getStaticPaths = async () => {
 
       return {
         paths,
-        fallback: false,
+        fallback: "blocking",
       }
      
 }
--- pages/property/[slug].tsx
+++ pages/property/[slug].tsx
@@ -138,7 +138,7 @@ export const getStaticPaths = async () => {
 
       return {
         paths,
-        fallback: false,
+        fallback: "blocking",
       }
      
 }
