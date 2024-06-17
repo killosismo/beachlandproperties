71e8dd790137ceb88f35e534c0b449a0612b2477
killosismo <killosismo>
2024-06-17 11:46:22 -0400

    adding bavaro 6

--- pages/bavaro.tsx
+++ pages/bavaro.tsx
@@ -24,18 +24,19 @@ const bavaro = ({ towns }: Props) => {
         <div className="mx-auto grid grid-cols-1 sm:grid-cols-1 lg:max-w-7xl lg:grid-cols-2 md:grid-cols-2 lg:px-8 pb-5">
           <div className="grid grid-cols-1 gap-4 sm:gap-6 lg:gap-4 pb-5">
             <img
-              src="https://cdn.sanity.io/images/lqpgcd61/production/ac95ca408164c7611bb25fbf99f65e60d91640ff-996x1024.jpg?rect=0,52,996,972"
+              src="https://cdn.sanity.io/images/lqpgcd61/production/bd4c235e665550304f8c15a3ef1150d24f99ec8a-728x455.jpg?rect=273,0,455,455"
               alt=""
               className="rounded-lg bg-gray-100" />
             
           </div>
           <div className=' px-8 grid grid-cols-1 sm:grid-cols-1 pb-5'>
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Cap Cana</h2>
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Bavaro</h2>
         
-              <p className="mt-4 text-sm text-gray-500">Welcome to Cap Cana, where luxury meets leisure in a tropical paradise that's as exclusive as it is enchanting. Nestled along the pristine eastern coast of the Dominican Republic, Cap Cana beckons travelers and investors with its irresistible blend of opulence, natural beauty, and endless possibilities.</p>
-              <p className="mt-2 text-sm text-gray-500">At the heart of Cap Cana lies its crown jewel, Juanillo Beach, a stretch of coastline so picturesque it seems plucked from a postcard. Beyond its chill beaches, Cap Cana boasts a plethora of world-class resorts and luxury accommodations, each offering unparalleled comfort and impeccable service.</p>
-              <p className="mt-2 text-sm text-gray-500">For the discerning traveler, Cap Cana presents an array of exclusive experiences and amenities. Golf enthusiasts can tee off at the acclaimed and awarded Punta Espada Golf Club, designed by the legendary Jack Nicklaus and offering breathtaking views of the Caribbean Sea at every turn. Meanwhile, adventurers can explore the rugged beauty of the Scape Park, where zip lining, cave exploration, and horseback riding await amidst lush tropical landscapes. And if your love for sport fishing has no limits you can embark on a deep-sea fishing excursion from Cap Cana’s own Marina in search of the big catch. The possibilities are as endless as the horizon.</p>
-              <p className="mt-2 text-sm text-gray-500">As a real estate destination, Cap Cana is a haven for investors seeking both luxury and value. From lavish oceanfront estates to chic condominiums with panoramic views, the market offers a diverse range of properties to suit every taste and budget. With a stable economy, favorable tax incentives, and a growing tourism industry, Cap Cana presents an unparalleled opportunity for those looking to invest in paradise. So come experience the magic of Cap Cana, where luxury knows no bounds, and paradise is just a heartbeat away.</p>
+              <p className="mt-4 text-sm text-gray-500">Bavaro is well known as Punta Cana’s counterpart as a premier tourist destination with its impressive array of beachfront and all-inclusive resorts and vacation rentals. Each day you can see all these vacationers take long walks at the beach or enjoy their favorite drink or meal at the various bars and restaurants near the renowned Playa Bavaro and Arena Gorda Beach.</p>
+              <p className="mt-2 text-sm text-gray-500">The Bavaro area is ideal for renters (short or long term) because everything is so accessible that is easy to feel adventurous having multiple daytime recreational spots like Caribbean Lake Park, The Dolphinarium or nighttime hotspots like Coco Bongo, El Patio and much more. You are also surrounded by Malls and convenient stores that make all effortless, like Downtown Mall and San Juan Mall.</p>
+              <p className="mt-2 text-sm text-gray-500">But Bavaro isn't just about relaxation and fun—it's also a savvy investment opportunity. It certainly is the economic engine of Punta Cana with its booming tourism industry, diverse businesses and each day growing more, Bavaro presents a wealth of options for those looking to invest in a piece of Caribbean paradise.</p>
+              <p className="mt-2 text-sm text-gray-500">The real estate market in Bavaro is as diverse as it is promising, where you can find from luxury beachfront townhouses and condominiums to commercial properties ripe for development, either under development or ready to use and profit right away. You can also find opportunities inside popular communities like the Cocotal Golf Residence, Los Corales or White Sands. </p>
+              <p className="mt-2 text-sm text-gray-500">So grab a Piña Colada, chill and stay tuned for updates, the best is yet to come to the area! </p>
 
             
           </div>
